1239b4f3d143a3e0a9567d8ada0bff4d234563c1
mitsuhiko-nozawa <mitsuhiko.0516.nozawa@gmail.com>
2021-12-09 03:41:30 +0900

    fix header and rebuild

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,8 +7,8 @@
                 <li><a href="#career">経歴</a></li>
                 <li><a href="#skill">スキル</a></li>
                 <li><a href="#contact">連絡先</a></li>
-                <li class="fa fa-twitter"> <a href="https://twitter.com/ShiningStars_2">Twitter</a></li>
-                <li class="fa fa-github"> <a href="https://github.com/mitsuhiko-nozawa">Github</a></li>
+                <li> <a href="https://twitter.com/ShiningStars_2"><span class="fa fa-twitter"></span>Twitter</a></li>
+                <li> <a href="https://github.com/mitsuhiko-nozawa"><span class="fa fa-github"></span>Github</a></li>
         </div>
     </header>
 </template>
@@ -29,7 +29,6 @@ header{
     position: fixed;
     top:0px;
     z-index: 10;
-    vertical-align: middle;
 }
 
 
@@ -37,16 +36,16 @@ header{
 .header-list li{
     list-style: none;
     float: left;
-    height:20px;
-    padding: 20px 30px;
-    display: inline-block;
-    vertical-align: middle;
     background-color: rgba(129, 232, 255, 0.824);
     box-align: center; 
-    transition: all 0.2s;
+    transition: all 0.3s;
 }
 .header-list a{
     color: black;
+    height:20px;
+    padding: 20px 30px;
+
+    display: inline-block;
 }
 
 
